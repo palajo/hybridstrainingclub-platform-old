573df76850af89bf5b5f4b01c21f3fefd944c618
Oleh Palazhii <palajoleg@gmail.com>
2024-01-26 10:32:38 +0200

    [update] dark mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,43 +1,52 @@
-import React from 'react';
-import { ConfigProvider } from 'antd';
+import React, { useCallback, useEffect, useState } from 'react';
+import { ConfigProvider, theme } from 'antd';
 import type { AppProps } from 'next/app';
 import RootLayout from '@/layouts/RootLayout';
 import '@/styles/globals.css';
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <ConfigProvider
-    theme={{
-      token: {
-        colorPrimary: '#17E4B1',
-      },
-      components: {
-        Button: {
-          colorPrimary: '#17E4B1',
-          algorithm: true, // Enable algorithm
-        },
-        Input: {
-          colorPrimary: '#17E4B1',
-          algorithm: true, // Enable algorithm
-        },
-        Layout: {
-          siderBg: '#0E2420',
-          triggerBg: '#0E3B32',
-        },
-        Menu: {
-          darkItemBg: '#0E2420',
-          darkSubMenuItemBg: '#0E3B32',
-          darkPopupBg: '#0E3B32',
-        },
-        Anchor: {
+const App = ({ Component, pageProps }: AppProps) => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    setDarkMode(window.matchMedia("(prefers-color-scheme:dark)").matches ? true : false);
+  }, []);
+
+  return (
+    <ConfigProvider
+      theme={{
+        algorithm: darkMode ? theme.darkAlgorithm : theme.defaultAlgorithm,
+        token: {
           colorPrimary: '#17E4B1',
         },
-      },
-    }}
-  >
-    <RootLayout>
-      <Component {...pageProps} />
-    </RootLayout>
-  </ConfigProvider>
-);
+        components: {
+          Button: {
+            colorPrimary: '#17E4B1',
+            algorithm: true, // Enable algorithm
+          },
+          Input: {
+            colorPrimary: '#17E4B1',
+            algorithm: true, // Enable algorithm
+          },
+          Layout: {
+            siderBg: '#0E2420',
+            triggerBg: '#0E3B32',
+          },
+          Menu: {
+            darkItemBg: '#0E2420',
+            darkSubMenuItemBg: '#0E3B32',
+            darkPopupBg: '#0E3B32',
+          },
+          Anchor: {
+            colorPrimary: '#17E4B1',
+          },
+        },
+      }}
+    >
+      <RootLayout>
+        <Component {...pageProps} />
+      </RootLayout>
+    </ConfigProvider>
+  );
+};
 
 export default App;
